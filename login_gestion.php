d2e32eee600f18c40ad838295db8415f7645be30
OrangeIUT <dorian.riberot.iut@gmail.com>
2024-06-11 14:55:16 +0200

    rename pages

--- login_gestion.php
+++ login_gestion.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>SAE23_données</title>
+    <title>SAE23 connexion gestion</title>
     <meta http-equiv="refresh" content="120" /> <!-- Will refresh page every 2 minutes -->
     <link rel="stylesheet" type="text/css" href="PAGE CSS" /> <!-- METTRE CSS ICI -->
 </head>
